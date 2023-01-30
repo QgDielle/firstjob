c2bf024f9516e6cd2672a3fc7adf8c0a4bb4ab41
Francesco Esposito <obpe91@gmail.com>
2023-01-30 16:45:03 +0100

    Bottone annuncio, redirect e form annuncio e categorie

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,15 @@
     </x-header>
 
 
+    @auth
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <a class="btn btn-primary" href="">Inserisci annuncio</a>
+
+            </div>
+        </div>
+    </div>
+    @endauth
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PublicController::class , 'welcome'])->name('welcome');
+
+Route::get('/article/create', [ArticleController::class , 'create'])->name('article.create');
