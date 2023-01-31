5e53dc1ca7091db247bbc8e1a3cca5570f3e2f82
giulia floris <giulia.floris93@libero.it>
2023-01-31 15:24:19 +0100

    sistemato error nella sezione categorie

--- app/Http/Livewire/CreateFormArticle.php
+++ app/Http/Livewire/CreateFormArticle.php
@@ -10,10 +10,9 @@ use Illuminate\Support\Facades\Auth;
 class CreateFormArticle extends Component
 {
     public $title, $description, $price, $category;
-    
-    
 
-    public function store(){
+    public function store()
+    {
         $this->validate();
         $category = Category::find($this->category);
         $category->articles()->create([
@@ -22,42 +21,41 @@ class CreateFormArticle extends Component
             'price' => $this->price,
             'user_id' => Auth::user()->id,
         ]);
-        
+
         $this->reset();
-        session()->flash('successMessage', 'Hai inserito correttamente inserito il tuo annuncio');
+        session()->flash('successMessage', 'Hai inserito correttamente il tuo annuncio');
     }
-    
+
     protected $rules = [
-        
-        'title'=> 'required|min:3|max:20',
+
+        'title' => 'required|min:3|max:20',
         'description' => 'required|min:3|max:2000',
+        'category' => 'required',
         'price' => 'required|numeric',
     ];
-    
+
     protected $messages = [
 
-        'title.required'=> 'Inserisci un titolo',
-        'title.min'=> 'Il titolo che hai inserito è inferiore di 3 caratteri',
-        'title.max'=> 'Il titolo che hai inserito è maggiore di 20 caratteri',
-        'description.required'=> 'Inserisci una descrizione dell articolo',
-        'description.min'=> 'La descrizione che hai inserito è inferiore di 3 caratteri',
-        'description.max'=> 'La descrizione che hai inserito è maggiore di 2000 caratteri',
-        'price.required'=> 'Inserisci il prezzo',
-        'price.numeric'=> 'Inserisci solo la cifra',
-
+        'title.required' => 'Inserisci un titolo',
+        'title.min' => 'Il titolo che hai inserito è inferiore di 3 caratteri',
+        'title.max' => 'Il titolo che hai inserito è maggiore di 20 caratteri',
+        'description.required' => 'Inserisci una descrizione dell articolo',
+        'description.min' => 'La descrizione che hai inserito è inferiore di 3 caratteri',
+        'description.max' => 'La descrizione che hai inserito è maggiore di 2000 caratteri',
+        'category.required' => 'Seleziona la categoria',
+        'price.required' => 'Inserisci il prezzo',
+        'price.numeric' => 'Inserisci solo la cifra',
 
     ];
-    
+
     public function updated($propertyName)
     {
         $this->validateOnly($propertyName);
     }
-    
-    
+
+
     public function render()
     {
         return view('livewire.create-form-article');
     }
-    
-    
 }
--- resources/views/livewire/create-form-article.blade.php
+++ resources/views/livewire/create-form-article.blade.php
@@ -1,42 +1,45 @@
 <div>
-
-    <form wire:submit.prevent='store'>
-        @csrf
-        <x-messages />
-        <div class="mb-3">
-            <label for="title" class="form-label @error('title') is-invalid @enderror">Titolo</label>
-            <input wire:model.lazy='title' type="text" class="form-control" id="title">
-            @error('title')
-            <span class="fst-italic small text-danger">{{$message}}</span>
-          @enderror
-        </div>
-        <div class="mb-3">
-            {{-- <label for="category" class="form-label">Seleziona Categorie</label> --}}
-            <select wire:model.defer='category' id="category" class="form-select" aria-label="Default select example">
-                <option selected >Seleziona Categoria</option>
-                @foreach ($categories as $category)
-                <option value="{{$category->id}}">{{$category->name}}</option>
-                @endforeach
-
-            </select>
-        </div>
-        <div class="mb-3">
-            <label for="description" class="form-label @error('description') is-invalid @enderror">Messaggio</label>
-            @error('description')
-            <span class="fst-italic small text-danger">{{$message}}</span>
-          @enderror
-            <textarea wire:model.lazy='description' class="form-control" id="description" rows="7" cols="30"></textarea>
-        </div>
-        <div class="mb-3">
-            <label for="price" class="form-label @error('price') is-invalid @enderror">Prezzo</label>
-            <input wire:model.lazy='price' type="number" class="form-control" id="price">
-            @error('price')
-            <span class="fst-italic small text-danger">{{$message}}</span>
-          @enderror
-        </div>
-
-        <button class="btn btn-outline-primary" type="submit">submit</button>
-
-    </form>
-
+  <form wire:submit.prevent='store'>
+    @csrf
+    <x-messages />
+    <div class="mb-3">
+      <label for="title" class="form-label @error('title') is-invalid @enderror">Titolo</label>
+      <input wire:model.lazy='title' type="text" class="form-control" id="title">
+      @error('title')
+      <span class="fst-italic small text-danger">{{$message}}</span>
+      @enderror
+    </div>
+    <div class="mb-3">
+      {{-- <label for="category" class="form-label">Seleziona Categorie</label> --}}
+      <select wire:model.defer='category' id="category" class="form-select @error('category') is-invalid @enderror">
+        
+        <option selected>Seleziona Categoria</option>
+        @foreach ($categories as $category)
+        <option value="{{$category->id}}">{{$category->name}}</option>
+        @endforeach
+        
+      </select>
+      @error('category')
+      <span class="fst-italic small text-danger">{{$message}}</span>
+      @enderror
+    </div>
+    <div class="mb-3">
+      <label for="description" class="form-label @error('description') is-invalid @enderror">Messaggio</label>
+      <textarea wire:model.lazy='description' class="form-control" id="description" rows="7" cols="30"></textarea>
+      @error('description')
+      <span class="fst-italic small text-danger">{{$message}}</span>
+      @enderror
+    </div>
+    <div class="mb-3">
+      <label for="price" class="form-label @error('price') is-invalid @enderror">Prezzo</label>
+      <input wire:model.lazy='price' type="number" class="form-control" id="price">
+      @error('price')
+      <span class="fst-italic small text-danger">{{$message}}</span>
+      @enderror
+    </div>
+    
+    <button class="btn btn-outline-primary" type="submit">submit</button>
+    
+  </form>
+  
 </div>
