d14a1d7ecb1d04a11d8ec173257bd261a1912081
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-01-31 15:07:43 +0100

    front-end pagina dettaglio con carousel

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -4,9 +4,35 @@
     </x-header>
     <div class="container my-5">
         <div class="row">
-            <div class="col-12 col-md-3 my-3">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://picsum.photos/300" class="card-img-top" alt="{{ $article->title }}">
+            <div class="col-12 col-md-6 my-3">
+                <div class="card">
+                    <div id="carouselExampleControls" class="carousel slide mb-3" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="https://picsum.photos/300" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://picsum.photos/300" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://picsum.photos/300" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6">
+                <div class="card h-100 align-items-center justify-content-around">
                     <div class="card-body">
                         <h5 class="card-title">{{ $article->title }}</h5>
                         <h5 class="card-title">{{ $article->category->name }}</h5>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,24 +13,30 @@
 
                 </div>
             </div>
-            <div class="row my-5">
-                @foreach ($articles as $article)
-                    <div class="col-12 col-md-3">
-                        <div class="card" style="width: 18rem;">
-                            <img src="https://picsum.photos/300" class="card-img-top" alt="{{ $article->title }}">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $article->category->name }}</h5>
-                                <h5 class="card-title">{{ $article->title }}</h5>
-                                <p class="card-text">{{ $article->price }}</p>
-                                <p class="card-text">{{ $article->description }}</p>
-                                <a class="btn btn-primary" href="{{ route('article.show', compact('article')) }}">Vai al
-                                    dettaglio del prodotto</a>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
         </div>
     @endauth
+    <div class="container">
+        <div class="row my-5">
+            @foreach ($articles as $article)
+                <div class="col-12 col-md-3">
+                    <div class="card" style="width: 18rem;">
+
+                        <img src="https://picsum.photos/300" class="card-img-top" alt="{{ $article->title }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $article->category->name }}</h5>
+                            <h5 class="card-title">{{ $article->title }}</h5>
+                            <p class="card-text">{{ $article->price }}</p>
+                            <p class="card-text">{{ $article->description }}</p>
+                            <a class="btn btn-primary" href="{{ route('article.show', compact('article')) }}">Vai al
+                                dettaglio del prodotto</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+
+
 
 </x-layout>
