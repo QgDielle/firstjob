17d8aed95e18a16ca32b68612ef15b4cbad2a778
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-02-06 18:15:26 +0100

    aggiunta la possibilita di aggiungere piu foto e modificata la pagina revisore e dettaglio

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -1,16 +1,38 @@
 <x-layout>
     <x-header>
-        {{$article->title}}
+        {{ $article->title }}
     </x-header>
     <div class="container d-flex justify-content-center">
-        <a class="btn btn-outline-refuse me-2" href="{{url()->previous()}}"><i class="bi bi-backspace"></i></a>
-        <a class="btn btn-outline-custom-r" href="{{route('article.create')}}">Aggiungi articolo</a>
+        <a class="btn btn-outline-refuse me-2" href="{{ url()->previous() }}"><i class="bi bi-backspace"></i></a>
+        <a class="btn btn-outline-custom-r" href="{{ route('article.create') }}">Aggiungi articolo</a>
     </div>
     <div class="container justify-content-around my-5">
         <div class="row align-items-center text-center">
             <div class="col-12 col-md-6">
-                <div>
-                    <div id="carouselExampleControls" class="carousel slide mb-3" data-bs-ride="carousel">
+                @if ($article->images)
+                    <div id="showCarousel" class="carousel slide " data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            @foreach ($article->images as $image)
+                                <div class="carousel-item @if ($loop->first) active @endif">
+                                    <img src="{{ Storage::url($image->path) }}" class="img-fluid p-3 rounded"
+                                        alt="">
+                                </div>
+                            @endforeach
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#showCarousel"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#showCarousel"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+
+                    </div>
+                @else
+                    <div id="showCarousel" class="carousel-slide " data-bs-ride="carousel">
                         <div class="carousel-inner">
                             <div class="carousel-item active">
                                 <img src="https://picsum.photos/300" class="d-block w-100" alt="...">
@@ -22,18 +44,18 @@
                                 <img src="https://picsum.photos/300" class="d-block w-100" alt="...">
                             </div>
                         </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                        <button class="carousel-control-prev" type="button" data-bs-target="#showCarousel"
                             data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Previous</span>
                         </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                        <button class="carousel-control-next" type="button" data-bs-target="#showCarousel"
                             data-bs-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
                         </button>
                     </div>
-                </div>
+                @endif
             </div>
             <div class="col-12 col-md-6">
                 <div>
