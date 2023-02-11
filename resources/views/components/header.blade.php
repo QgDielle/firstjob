8217b2080a678d8276b44f672c4c9b30be7b9c6f
Giangabriele De Luca <giangabriele.deluca@gmail.com>
2023-02-11 21:00:00 +0100

    creata mission e risolto problemi

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
-<div class="container-fluid my-5 text-white">
-    {{-- <div class="container my-5 p-2 text-white"> --}}
+<div class="container-fluid my-5 pt-2 text-white">
+    <div class="container my-5 p-2 text-white">
         <div class="row bg-custom1 rounded-2 ">
             <div class="col-12 ">
                 <h1 class="text-center display-3 p-3">{{$slot}}</h1>
--- resources/views/revisor/contact-us.blade.php
+++ resources/views/revisor/contact-us.blade.php
@@ -73,7 +73,7 @@
                             <h3>
                                 <a href="https://www.linkedin.com/in/alfredo-coppola-953365262/" target="_blank">Alfredo Coppola</a>
                             </h3>
-                        <p>Aulab Student and</p><br>
+                            <p>Aulab Student and</p><br>
                             <p><b> Junior Full-Stack Developer</b></p>
                         </div>
                     </div>
@@ -91,7 +91,7 @@
                             <h3>
                                 <a href="https://www.linkedin.com/in/francesco-esposito-824b9b264" target="_blank">Francesco Esposito</a>
                             </h3>
-                           <p>Aulab Student and</p><br>
+                            <p>Aulab Student and</p><br>
                             <p><b> Junior Full-Stack Developer</b></p>
                         </div>
                     </div>
@@ -101,7 +101,7 @@
                 <div class="card">
                     <div class="face face1">
                         <div class="content p-1">
-                            <img src="https://picsum.photos/300/200" alt="">
+                            <img src="../media/thisisus/giulia.png" alt="">
                         </div>
                     </div>
                     <div class="face face2">
@@ -119,6 +119,24 @@
 
 
         <x-header>
+            Mission
+        </x-header>
+        <div class="container my-5">
+            <div class="row justify-content-center">
+                <p class="mission-style">Un portale di annunci che da la possibilità ai suoi utenti di vendere ed acquistare oggetti di seconda mano di qualsiasi tipo, riducendo così l’impatto ambientale sulle cose che buttiamo e dargli una seconda vita.</p>
+
+                <p class="mission-style">
+                    Perché è importante il SecondHand? Prima di tutto aiuti l’ambiente: ogni prodotto riutilizzato abbassa l’impatto ambientale di una elevata produzione di prodotti che inquinano la nostra Terra.
+                </p>
+                <p class="mission-style">
+                    In secondo luogo il costo: un prodotto SecondHand costa meno e se tenuto bene può durare ulteriori anni. Perché presto.it? Perché i nostri revisori si assicurano che i prodotti messi in vendita nel nostro portale siano sicuri, in buoni condizioni e rispettino requisiti ecologici ambientali atti a migliorare la nostra qualità della vita.
+                </p>
+            </div>
+        </div>
+
+
+
+        <x-header>
             {{ __('ui.workWithUs') }}
         </x-header>
 
@@ -132,7 +150,7 @@
                             <label for="message" class="form-label"></label>
                             <textarea name="message" class="form-control" id="message" rows="3" cols="70"> {{ __('ui.mess') }}</textarea>
                         </div>
-                        <div class="mb-3 form-check">
+                        <div class="mb-3 form-check text-black">
                             <input type="checkbox" class="form-check-input" id="checkbox">
                             <label class="form-check-label" for="checkbox"> {{ __('ui.becomeR') }}</label>
                         </div>
